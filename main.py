5350ae59548318f50cba0f69bb961a7b11ad9d07
Giovannibriglia <giovanni.briglia@unimore.it>
2025-10-01 10:52:23 +0200

    update main

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def get_args():
         help="Environment suite to benchmark (e.g. gymnasium, vmas, pettingzoo)",
     )
     parser.add_argument(
-        "--n_episodes_train", type=int, default=250, help="Number of training episodes"
+        "--n_episodes_train", type=int, default=2500, help="Number of training episodes"
     )
     parser.add_argument(
         "--n_checkpoints",
         type=int,
-        default=25,
+        default=50,
         help="Number of checkpoints to save during training",
     )
     parser.add_argument(
@@ -30,10 +30,10 @@ def get_args():
         help="Rollout length per environment per update",
     )
     parser.add_argument(
-        "--n_train_envs", type=int, default=16, help="Number of training environments"
+        "--n_train_envs", type=int, default=12, help="Number of training environments"
     )
     parser.add_argument(
-        "--n_eval_envs", type=int, default=16, help="Number of evaluation environments"
+        "--n_eval_envs", type=int, default=12, help="Number of evaluation environments"
     )
     parser.add_argument("--seed", type=int, default=42, help="Random seed")
     parser.add_argument(
